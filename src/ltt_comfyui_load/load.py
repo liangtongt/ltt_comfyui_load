f94ad30c8ba772c073acbc9b41ce34c09b994d80
ltt147 <394115207@qq.com>
2024-05-08 14:07:34 +0800

    oo

--- src/ltt_comfyui_load/load.py
+++
@@ -1,8 +0,0 @@
-import os
-import urllib.request
-# import importlib.util
-
-def load(n):
-    url = f'https://github.com/liangtongt/ltt_comfyui_wf/raw/main/{n}.enj'
-    urllib.request.urlretrieve(url, os.path.basename(url)[:-3]+'pyc')
-    __import__(n)
